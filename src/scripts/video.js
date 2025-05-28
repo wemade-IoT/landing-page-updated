310cd3be3765b027b27fb9749fe449675c06bdd4
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-28 00:25:32 -0500

    refactor: standardize quotes and improve iframe dimensions for modal

--- src/scripts/video.js
+++ src/scripts/video.js
@@ -1,42 +1,35 @@
-const videoContainer = document.querySelector('#about-us .main-image');
+const videoContainer = document.querySelector("#about-us .main-image");
 
-videoContainer.addEventListener('click', (e) => {
-  const modal = document.createElement('div');
-  modal.className = 'modal';
+videoContainer.addEventListener("click", (e) => {
+  const modal = document.createElement("div");
+  modal.className = "modal";
 
   modal.innerHTML = `
     <button class="modal-close">&times;</button>
     <div class="modal-content modal-pop">
-      <iframe 
-        width="90%" height="80%" 
-        style="min-width:320px; min-height:200px; max-width:1200px; max-height:800px; border-radius:8px;"
-        src="https://www.youtube.com/embed/spMcHGHjUKk?si=rerUmRXQwt32v6If"
-        title="YouTube video player" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-      </iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/VFVLQwdlmRs?si=M45GpVej2mhvEX_C" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   `;
 
-  document.body.style.overflow = 'hidden';
+  document.body.style.overflow = "hidden";
 
   document.body.appendChild(modal);
 
-  const modalContent = modal.querySelector('.modal-content');
+  const modalContent = modal.querySelector(".modal-content");
   setTimeout(() => {
-    modalContent.classList.add('modal-pop--active');
+    modalContent.classList.add("modal-pop--active");
   }, 10);
 
-  const closeBtn = document.querySelector('.modal-close');
-  closeBtn.addEventListener('click', () => {
+  const closeBtn = document.querySelector(".modal-close");
+  closeBtn.addEventListener("click", () => {
     modal.remove();
-    document.body.style.overflow = '';
+    document.body.style.overflow = "";
   });
 
-  modal.addEventListener('click', (event) => {
+  modal.addEventListener("click", (event) => {
     if (event.target === modal) {
       modal.remove();
-      document.body.style.overflow = '';
+      document.body.style.overflow = "";
     }
   });
 });
