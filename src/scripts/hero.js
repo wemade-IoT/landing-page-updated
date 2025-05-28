fbf1c805d2eb5449010a67cb6be7dd12916ea37a
Nicolas Esteban <u202217485@upc.edu.pe>
2025-05-28 00:20:04 -0500

    feat: enhance hero section with background image transitions and preload images

--- src/scripts/hero.js
+++ src/scripts/hero.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const heroSection = document.getElementById("hero");
   const heroImage = document.querySelector(".hero__image");
+
+  const backgrounds = [
+    "/images/hero-background.png",
+    "/images/hero-background-alpha.webp"
+  ];
   const images = [
     "/images/farmer2.png",
-    "/images/business.png",
+    "/images/negro.webp"
   ];
+
   let currentIndex = 0;
+
+  backgrounds.forEach(src => {
+    const img = new Image();
+    img.src = src;
+  });
+  images.forEach(src => {
+    const img = new Image();
+    img.src = src;
+  });
 
   setInterval(() => {
     currentIndex = (currentIndex + 1) % images.length;
@@ -14,5 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
       heroImage.src = images[currentIndex];
       heroImage.style.opacity = "1";
     }, 400);
+
+    heroSection.style.transition = "background-image 0.5s ease-in-out";
+    heroSection.style.backgroundImage = `url('${backgrounds[currentIndex]}')`;
   }, 5000);
 });
