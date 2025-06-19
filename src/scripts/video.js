15a6ca3594a9d787a845ba5901ebbcc228c3f5ba
Nicolas Esteban <u202217485@upc.edu.pe>
2025-06-19 04:34:54 -0500

    feat: add video modal functionality and styles for team introduction

--- src/scripts/video.js
+++ src/scripts/video.js
@@ -1,18 +1,28 @@
 const videoContainer = document.querySelector("#about-us .main-image");
+const videoTeam = document.querySelector("#about-team .about-team__video-btn");
 
-videoContainer.addEventListener("click", (e) => {
+function createVideoModal(videoSrc, title = "YouTube video player") {
   const modal = document.createElement("div");
   modal.className = "modal";
 
   modal.innerHTML = `
     <button class="modal-close">&times;</button>
     <div class="modal-content modal-pop">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/VFVLQwdlmRs?si=M45GpVej2mhvEX_C" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe 
+        width="1200" 
+        height="675" 
+        src="${videoSrc}" 
+        title="${title}" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen
+        style="max-width: 90vw; max-height: 80vh; border-radius: 8px;">
+      </iframe>
     </div>
   `;
 
   document.body.style.overflow = "hidden";
-
   document.body.appendChild(modal);
 
   const modalContent = modal.querySelector(".modal-content");
@@ -20,7 +30,7 @@ videoContainer.addEventListener("click", (e) => {
     modalContent.classList.add("modal-pop--active");
   }, 10);
 
-  const closeBtn = document.querySelector(".modal-close");
+  const closeBtn = modal.querySelector(".modal-close");
   closeBtn.addEventListener("click", () => {
     modal.remove();
     document.body.style.overflow = "";
@@ -32,4 +42,12 @@ videoContainer.addEventListener("click", (e) => {
       document.body.style.overflow = "";
     }
   });
+}
+
+videoContainer.addEventListener("click", (e) => {
+  createVideoModal("https://www.youtube.com/embed/xdOzNHovALA?si=ABe2IalnMgfw3quq", "YouTube video player");
+});
+
+videoTeam.addEventListener("click", (e) => {
+  createVideoModal("https://www.youtube.com/embed/2BaLgVPwyto?si=_Xoscg3pocRz9Oh2", "Meet Our Team - YouTube video player");
 });
